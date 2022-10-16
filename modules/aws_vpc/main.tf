d6700cb982276b57eb77e1b043c151a3f9e4d079
Fabricio <carboni@gmail.com>
2022-10-17 00:09:47 +0100

    Improvements

--- modules/aws_vpc/main.tf
+++ modules/aws_vpc/main.tf
@@ -166,3 +166,24 @@ resource "aws_nat_gateway" "nat-gw-b" {
   }
 }
 
+/*
+* Outputs that will be passed to aws_alb module
+* and be inputed in inputs block in aws_alb/terragrunt.hcl
+*/
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnets"{
+  description = "Public subnets"
+  value = [aws_subnet.public-a.id,aws_subnet.public-b.id]
+  # value = aws_subnet.public-a.id
+}
+
+output "private_subnets"{
+  description = "Private subnets"
+  value = [aws_subnet.private-a.id,aws_subnet.private-b.id]
+}
+
